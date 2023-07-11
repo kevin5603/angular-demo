e7bde8986a8e99d3c6b7f154977fc33972bd00c1
kevin5603 <32038795+kevin5603@users.noreply.github.com>
2023-07-11 15:02:53 +0800

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  coverageDirectory: '<rootDir>/target/test-result',
+  reporters: [
+    'default',
+    ['jest-junit', {output: '<rootDir>/target/test-results/TEST-results-jext.xml'}],
+    testResultsProcessor: 'jest-teamcity-reporter',
+    testMatch: ['<rootDir>/test/app/**/*spec.ts']
+  ]
+}
