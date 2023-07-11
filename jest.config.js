d3b6d2c367457db9aa6228849e5990457dcee20d
kevin5603 <32038795+kevin5603@users.noreply.github.com>
2023-07-11 15:04:52 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
   reporters: [
     'default',
     ['jest-junit', {output: '<rootDir>/target/test-results/TEST-results-jext.xml'}],
-    testResultsProcessor: 'jest-teamcity-reporter',
-    testMatch: ['<rootDir>/test/app/**/*spec.ts']
+  ],
+  testResultsProcessor: 'jest-teamcity-reporter',
+  testMatch: ['<rootDir>/test/app/**/*spec.ts']
   ]
 }
